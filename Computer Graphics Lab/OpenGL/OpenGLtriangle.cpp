fff7b99532755e0c76899f219787c406f382e957
Sulav Baral <sulavbaral58@gmail.com>
2022-11-06 20:35:30 +0545

    open gl programs

---
+++ Computer Graphics Lab/OpenGL/OpenGLtriangle.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+
+void init (void)
+{
+    glClearColor (1.0, 1.0, 1.0, 0.0); // last value is alpha (transparency)
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 200.0, 0.0, 150.0); //orthographic projection
+}
+
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.5, 0.0, 0.5);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+        glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE); // Single frame buffer
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+    return 0;
+}
