9e610ee1a46a7da7c616a7c45c52d9ddfdef4e63
tyzrex <sulavbaral58@gmail.com>
2023-01-13 09:59:43 +0545

    polygon table program

---
+++ Computer Graphics Lab/3D-Object Representation/Polygontable.cpp
@@ -0,0 +1,39 @@
+#include <graphics.h>
+#include <stdlib.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int ver, face, n;
+	
+	int gdriver= DETECT, gmode;
+	initgraph(&gdriver, &gmode, "NULL");
+	
+  cout<<"Enter the number of faces: ";
+  cin>>face;
+  n=face;
+	
+	for(int j=0; face!=j; face--){
+		printf("Enter the total number of vertex in face %d: ", n-face+1);
+		scanf("%d", &ver);
+		int poly[ver], i;
+		
+		printf("Enter vertex from face %d (x,y): \n", n-face+1);
+		for(i=0; i<(ver*2); i++){
+			printf("\tx%d , y%d : ", i/2,i/2);
+			scanf("%d %d", &poly[i], &poly[i+1]);
+			i++;
+		}
+		poly[ver*2]=poly[0];
+		poly[(ver*2)+1]=poly[1];
+//		face=face-1;
+		setcolor(getmaxcolor());
+		setfillstyle(SOLID_FILL, n-face+1);
+		fillpoly(ver+1, poly);
+	}
+	
+	getch();
+	closegraph();
+	return 0;
+}
