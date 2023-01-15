d8d185aa868c0d2601aca25c2f7db724e4d1d94c
tyzrex <sulavbaral58@gmail.com>
2023-01-15 20:09:22 +0545

    toh

--- Data Structures and Algorithms/Practical/11TowerofHanoi.cpp
+++ Data Structures and Algorithms/Practical/11TowerofHanoi.cpp
@@ -6,9 +6,9 @@ void towerOfHanoi(int n, char source, char auxiliary, char destination){
     if(n == 0){
         return;
     }
-    towerOfHanoi(n-1, source, destination, auxiliary);
+    towerOfHanoi(n-1,source,auxiliary,destination);
     cout<<"Move disk "<<n<<" from "<<source<<" to "<<destination<<endl;
-    towerOfHanoi(n-1, auxiliary, source, destination);
+    towerOfHanoi(n-1,auxiliary,destination,source);
 }
 
 int main(){
