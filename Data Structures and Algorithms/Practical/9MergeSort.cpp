369c12620ca0b342ed33082d633ba3f7a37dc6e0
tyzrex <sulavbaral58@gmail.com>
2023-01-16 05:14:16 +0545

    corrections

--- Data Structures and Algorithms/Practical/9MergeSort.cpp
+++ Data Structures and Algorithms/Practical/9MergeSort.cpp
@@ -4,55 +4,74 @@
 
 using namespace std;
 
-void merge(int a[], int low, int mid, int high)
-{
-    int i, j, k, c[50];
-    i = low;
-    k = low;
-    j = mid + 1;
+#include <iostream>
+using namespace std;
 
-    while (i <= mid && j <= high)
-    {
-        if (a[i] < a[j])
-        {
-            c[k] = a[i];
-            k++;
+// Function to merge two subarrays of arr[]
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
         }
-        else
-        {
-            c[k] = a[j];
-            k++;
+        else {
+            arr[k] = R[j];
             j++;
         }
-    }
-    while (i <= mid)
-    {
-        c[k] = a[i];
         k++;
+    }
+
+    // Copy the remaining elements of L[], if there
+    // are any
+    while (i < n1) {
+        arr[k] = L[i];
         i++;
-    }
-    while (j <= high)
-    {
-        c[k] = a[j];
         k++;
-        j++;
     }
-    for (i = low; i < k; i++)
-    {
-        a[i] = c[i];
+
+    // Copy the remaining elements of R[], if there
+    // are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
 
-void mergesort(int a[], int low, int high)
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
 {
-    int mid;
-    if (low < high)
-    {
-        mid = (low + high) / 2;
-        mergesort(a, low, mid);
-        mergesort(a, mid + 1, high);
-        merge(a, low, mid, high);
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
     }
 }
 
@@ -67,7 +86,7 @@ int main()
     {
         cin >> a[i];
     }
-    mergesort(a, 0, n - 1);
+    mergeSort(a, 0, n - 1);
     cout << "Sorted array: ";
     for (i = 0; i < n; i++)
     {
